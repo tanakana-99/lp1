99a082c1e839e58e6fa4213f7df1af4175041fc6
tanakana-99 <kanakon527@gmail.com>
2025-04-28 12:19:54 +0900

    first commit

---
+++ assets/script/main.js
@@ -0,0 +1,16 @@
+// ハンバーガーメニュー
+var hamburger = $('.burger-menu');
+
+$('.burger-menu__btn').on('click', function () {
+  hamburger.toggleClass('burger-menu-active');
+});
+
+$(window).on('resize', function () {
+  hamburger.removeClass('burger-menu-active');
+});
+
+// タブ
+$('#smarttab').smartTab({
+  enableUrlHash: false,
+  autoAdjustHeight: false
+});
